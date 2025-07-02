662a18505fe65b083f8c14b0c132eaceccff8ed9
Hadi Akbarzadeh <elsiomcoder@gmail.com>
2025-07-03 00:27:13 +0330

    Dusted

--- AutoCopyUSB/Config.cs
+++ AutoCopyUSB/Config.cs
@@ -5,8 +5,7 @@
         public const string DEFAULT_PASSWORD = "1";
         public const string PASSPORT_FILE_NAME = "Passport";
 
-        public const string DEVELOPER_URL_TITLE = "NABEGHE";
-        public const string SOFTWARE_URL = "https://elatel.ir/me/redirect/index.php?target=AutoCopyUSB";
-        public const string UPDATE_URL = "https://elatel.ir/me/updates/win/AutoCopyUSB.xml";
+        public const string DEVELOPER_URL_TITLE = "https://elatel.ir";
+        public const string SOFTWARE_URL = "https://elatel.ir/me/redirect/index.php?target=product-auto-copy-usb-desktop";
     }
 }
--- AutoCopyUSB/Form1_WndProc.cs
+++ AutoCopyUSB/Form1_WndProc.cs
@@ -14,6 +14,7 @@ namespace AutoCopyUSB
                     Stop();
                 }
             }
+
             base.WndProc(ref m);
         }
     }
